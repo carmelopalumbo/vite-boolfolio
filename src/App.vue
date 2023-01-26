d9225c16da1f3a27a4ea2db209ab7550dc3315d1
Carmelo Palumbo <carmelopalumbo@outlook.it>
2023-01-26 15:22:04 +0100

    add-api-call

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,36 @@
 import axios from "axios";
 
 export default {
+  name: "App",
 
+  data(){
+    return{
+      apiUrl: 'http://127.0.0.1:8000/api/projects',
+      projects: []
+    }
+  },
+
+  methods: {
+    getApi(){
+      axios.get(this.apiUrl)
+      .then(result => {
+        console.log(result.data);
+        this.projects = result.data.projects;
+      })
+    }
+  },
+
+  mounted(){
+    this.getApi();
+  }
 }
 </script>
 
 <template>
-  <h1>INIT</h1>
+  <h1>TEST API</h1>
+  <div v-for="project in projects" :key="project.id">
+   <p>{{ project.name }}</p>
+  </div>
 </template>
 
 <style>
